19600db302c3d626bfb54de068bdf2976e359494
cristiano-s-r-filho <cristiano.sr.filho@proton.me>
2026-02-25 13:08:07 -0300

    chore(db): add professional comments to 20260225123000_add_required_tier_to_classes.sql migration

--- cadmus-kernel/migrations/20260225123000_add_required_tier_to_classes.sql
+++ cadmus-kernel/migrations/20260225123000_add_required_tier_to_classes.sql
@@ -1,2 +1,4 @@
+-- Migration to add the 'required_tier' column to the `classes` table.
+-- This column specifies an optional access tier required for documents of a certain class.
 ALTER TABLE classes
 ADD COLUMN required_tier TEXT NULL;
