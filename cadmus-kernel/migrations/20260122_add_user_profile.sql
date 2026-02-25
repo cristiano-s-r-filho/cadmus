2036b49f6dc6c15364e963202d303af937f5fb1d
cristiano-s-r-filho <cristiano.sr.filho@proton.me>
2026-02-25 12:54:35 -0300

    chore(db): add professional comments to 20260122_add_user_profile.sql migration

--- cadmus-kernel/migrations/20260122_add_user_profile.sql
+++ cadmus-kernel/migrations/20260122_add_user_profile.sql
@@ -1,2 +1,7 @@
+-- Adds user profile fields to the `users` table.
+
+-- Adds a column to store the URL of the user's avatar image.
 ALTER TABLE users ADD COLUMN avatar_url TEXT;
+
+-- Adds a column to store a recovery key for password reset or account recovery.
 ALTER TABLE users ADD COLUMN recovery_key TEXT;
