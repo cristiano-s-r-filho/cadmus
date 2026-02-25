56fd4a8dd5654acb41ffe0405597e9bd3418d00a
cristiano-s-r-filho <cristiano.sr.filho@proton.me>
2026-02-25 12:59:15 -0300

    chore(db): add professional comments to 20260127_add_trial.sql migration

--- cadmus-kernel/migrations/20260127_add_trial.sql
+++ cadmus-kernel/migrations/20260127_add_trial.sql
@@ -1,1 +1,3 @@
+-- Adds a 'trial_expires_at' column to the `users` table.
+-- This column is used to manage user trial periods, indicating when a free trial account expires.
 ALTER TABLE users ADD COLUMN IF NOT EXISTS trial_expires_at TIMESTAMP;
